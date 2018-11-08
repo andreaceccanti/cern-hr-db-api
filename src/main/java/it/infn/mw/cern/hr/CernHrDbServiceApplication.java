f5a838c27fd59e9352983e37eb74d7cab98cca03
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-11-08 21:18:23 +0100

    First import

---
+++ src/main/java/it/infn/mw/cern/hr/CernHrDbServiceApplication.java
@@ -0,0 +1,12 @@
+package it.infn.mw.cern.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CernHrDbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CernHrDbServiceApplication.class, args);
+	}
+}
